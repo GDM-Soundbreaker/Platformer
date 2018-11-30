4ebe49b92caa76001ac9533dbcbdf26df1a31787
GDM-Soundbreaker <43067364+GDM-Soundbreaker@users.noreply.github.com>
2018-11-30 14:28:39 +0000

    Added title and game over screens

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this allows us to use the scene loading function
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+
+    //this will be called by the button component when clicked
+    public void StartGame()
+    {
+        //Reset score
+        PlayerPrefs.DeleteKey("score");
+
+        //Load level
+        SceneManager.LoadScene("Level1");
+    }
+	
+}
---
+++ Assets/Scripts/TitleButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class TitleButton : MonoBehaviour {
+
+    //called when title button is clicked
+    public void BackToTitle()
+    {
+        // Return to title scene
+        SceneManager.LoadScene("Title");
+    }
+
+	
+}
